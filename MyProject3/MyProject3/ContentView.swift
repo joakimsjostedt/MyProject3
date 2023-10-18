8640b1da717faefd43a3bcceb82d051389f84088
Joakim Sjöstedt <joakim.sjostedt@softhouse.se>
2023-10-18 07:34:22 +0200

    Basic setup

---
+++ MyProject3/MyProject3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyProject3
+//
+//  Created by Joakim Sjöstedt on 2023-10-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyProject3/MyProject3/MyProject3App.swift
@@ -0,0 +1,17 @@
+//
+//  MyProject3App.swift
+//  MyProject3
+//
+//  Created by Joakim Sjöstedt on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct MyProject3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
